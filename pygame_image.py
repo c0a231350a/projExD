923184f58132933d24c64a16a7c83550ffe5c36f
yuma <c0a231350a@edu.teu.ac.jp>
2024-09-24 16:22:37 +0900

    練習8-3,4,5

--- pygame_image.py
+++ pygame_image.py
@@ -20,6 +20,15 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,+1))
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1,0))
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0]) #画像貼り付け
         screen.blit(bg_img2, [x+1600,0])
