47682bc66d56fb0eea3582e889b4d033094028dd
yuma <c0a231350a@edu.teu.ac.jp>
2024-09-24 15:21:52 +0900

    a

---
+++ pygame_image.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん") #ゲームのタイトル
+    screen = pg.display.set_mode((800, 600)) #横縦の大きさ
+    clock  = pg.time.Clock() 
+    bg_img = pg.image.load("fig/pg_bg.jpg") #画像読み込み
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0]) #画像貼り付け
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
