db98b43bda97989404bfe35d973910933f32d6a0
yuma <c0a231350a@edu.teu.ac.jp>
2024-09-24 16:03:27 +0900

    練習7-2

--- pygame_image.py
+++ pygame_image.py
@@ -17,9 +17,11 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%1600)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0]) #画像貼り付け
         screen.blit(bg_img2, [x+1600,0])
+        screen.blit(bg_img, [x+3200, 0]) 
+        screen.blit(bg_img2, [x+4800,0])
         screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
