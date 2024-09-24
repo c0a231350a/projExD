97392ee03301ae48363f8effe5ca1899056a769d
yuma <c0a231350a@edu.teu.ac.jp>
2024-09-24 17:10:58 +0900

    演習課題

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -21,14 +21,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        a = 0
+        b = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            b = -1
         elif key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
+            b = 1
         elif key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            a = -1
         elif key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            a = 2     
+        kk_rct.move_ip(a-1,b+0)
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0]) #画像貼り付け
         screen.blit(bg_img2, [x+1600,0])
