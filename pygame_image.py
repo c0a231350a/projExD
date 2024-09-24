917eb4120e39dc7b91b27aef001205ddf7c4f6f8
yuma <c0a231350a@edu.teu.ac.jp>
2024-09-24 15:31:10 +0900

    練習４

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600)) #横縦の大きさ
     clock  = pg.time.Clock() 
     bg_img = pg.image.load("fig/pg_bg.jpg") #画像読み込み
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #画像貼り付け
+        screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
